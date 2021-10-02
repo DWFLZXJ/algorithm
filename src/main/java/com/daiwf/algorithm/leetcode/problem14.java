536730c465313aef47caf9ae2f3b729a52de116d
戴文峰 <997940675@qq.com>
2021-10-02 15:24:20 +0800

    完善350题的两种解法

---
+++ src/main/java/com/daiwf/algorithm/leetcode/problem14.java
@@ -0,0 +1,14 @@
+package com.daiwf.algorithm.leetcode;
+
+/**
+ * @version [版本号，2021/10/2 0002]
+ * @文件名 problem14
+ * @作者 daiwf
+ * @创建时间 2021/10/2 0002 15:23
+ * @描述 [https://leetcode-cn.com/problems/longest-common-prefix/]
+ * 最长公共前缀
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class problem14 {
+}
--- src/main/java/com/daiwf/algorithm/leetcode/problem350.java
+++ src/main/java/com/daiwf/algorithm/leetcode/problem350.java
@@ -10,12 +10,19 @@ import java.util.Map;
  * @文件名 Intersect
  * @作者 daiwf
  * @创建时间 2021/10/2 0002 8:27
- * @版权 Copyright Epoint Tech. Co. Ltd. All Rights Reserved.
  * @描述 [ 参考题解：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/solution/liang-ge-shu-zu-de-jiao-ji-ii-by-leetcode-solution/]
  * @see [相关类/方法]
  * @since [产品/模块版本]
  */
 public class problem350 {
+
+    /**
+     * @作者 daiwf
+     * @创建时间 2021/10/2 0002 9:34
+     * @功能描述 [利用hashmap实现]
+     * * @param null
+     * @return:
+     */
     public int[] intersect(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length) {
             return intersect(nums2, nums1);
@@ -39,5 +46,39 @@ public class problem350 {
         }
         return Arrays.copyOfRange(sections, 0, index);
     }
+
+    /**
+     * @作者 daiwf
+     * @创建时间 2021/10/2 0002 9:37
+     * @功能描述 [双指针做法]
+     * * @param null
+     * @return:
+     */
+    public int[] intersectdoublePointer(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+       int length = Math.min(nums1.length,nums2.length);
+       int length1=nums1.length;
+       int length2=nums2.length;
+       int[] sections = new  int[length];
+       int index1=0,index2=0,index=0;
+       while (index1<length1&&index2<length2){
+           if(nums1[index1]<nums2[index2]){
+               index1++;
+           }
+           else if(nums1[index1]>nums2[index2]){
+               index2++;
+           }else {
+               sections[index++]=nums1[index1];
+               index1++;
+               index2++;
+           }
+
+       }
+       return Arrays.copyOfRange(sections,0,index);
+    }
+
+
+
 
 }
