cddc095ca513d9a762274dbc5b41715c42675b64
戴文峰 <997940675@qq.com>
2021-10-29 22:44:14 +0800

    阶段性提交，实现了二分搜索树的添加元素，前序遍历，中序遍历，后序遍历。层序遍历。

--- src/main/java/com/daiwf/algorithm/datastructure/BST.java
+++ src/main/java/com/daiwf/algorithm/datastructure/BST.java
@@ -1,16 +1,26 @@
 package com.daiwf.algorithm.datastructure;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import org.w3c.dom.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
- * @description: 实现一个二分搜索树
+ * @description:
  * @author: daiwf
- * @time: 2021-10-17
+ * @time: 2021-10-29
  */
 public class BST<E extends Comparable<E>> {
 
+    private Node root;
+    private int size;
+
     private class Node {
         public E e;
-        public Node left, right;
+        //相当于两个指向左右子树的指针
+        public Node right, left;
 
         public Node(E e) {
             this.e = e;
@@ -19,12 +29,9 @@ public class BST<E extends Comparable<E>> {
         }
     }
 
-    private Node root;
-    private int size;
-
     public BST() {
+        size = 0;
         this.root = null;
-        this.size = 0;
     }
 
     public int size() {
@@ -35,35 +42,37 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
+    /**
+     * 向二分搜索树中添加新的元素
+     */
     public void add(E e) {
-
         root = add(root, e);
-
-
     }
 
-    private Node add(Node node, E e) {
-
+    public Node add(Node node, E e) {
         if (node == null) {
             size++;
             return new Node(e);
         }
-
         if (e.compareTo(node.e) < 0) {
             node.left = add(node.left, e);
-        } else {
+        }
+        if (e.compareTo(node.e) > 0) {
             node.right = add(node.right, e);
         }
         return node;
-
     }
 
-    private boolean contains(E e) {
+
+    /**
+     * 判断二分搜索树中是否有元素
+     */
+    public boolean contains(E e) {
         return contains(root, e);
-
     }
 
-    private boolean contains(Node node, E e) {
+
+    public boolean contains(Node node, E e) {
         if (node == null) {
             return false;
         }
@@ -72,22 +81,107 @@ public class BST<E extends Comparable<E>> {
         }
         if (e.compareTo(node.e) < 0) {
             return contains(node.left, e);
-        } else {
+        }
+        if (e.compareTo(node.e) > 0) {
             return contains(node.right, e);
         }
+        return false;
+
     }
 
+    /**
+     * 二分搜索树的前序遍历
+     */
     public void preOrder() {
         preOrder(root);
     }
 
     public void preOrder(Node node) {
-        if (node != null) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+
+    }
+
+    /**
+     * 二分搜索树的中序遍历,中序遍历的结果一般就是排序的结果
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    public void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+
+
+    }
+
+    /**
+     * 二分搜索树的后序遍历
+     */
+    public void postOrder() {
+        postOrder(root);
+
+    }
+
+    public void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+
+    }
+
+    /**
+     * 非递归的前序遍历
+     */
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
             System.out.println(node.e);
-            preOrder(node.left);
-            preOrder(node.right);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+    }
+
+    /**
+     * 二分搜索树的层序遍历
+     * 层序遍历使用了队列这个结构
+     * 需要细细体会中间的巧妙
+     */
+    public void levelOrder() {
+        Queue<Node> queue = new LinkedList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            Node cur = queue.remove();
+            System.out.println(cur.e);
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
         }
 
     }
-}
 
+
+}
--- src/main/java/com/daiwf/algorithm/datastructure/Main.java
+++ src/main/java/com/daiwf/algorithm/datastructure/Main.java
@@ -6,4 +6,24 @@ package com.daiwf.algorithm.datastructure;
  * @time: 2021-10-17
  */
 public class Main {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST();
+        int[] nums = new int[]{2, 5, 10, 3, 1, 9, 4};
+        for (int num : nums) {
+            bst.add(num);
+        }
+
+       if( bst.contains(5)){
+           System.out.println("有这个元素");
+       }
+
+        bst.preOrder();
+        System.out.println("++++++++++++++++");
+        bst.preOrderNR();
+        System.out.println("================");
+        bst.inOrder();
+        System.out.println(">>>>>>>>>>>>.");
+        bst.postOrder();
+
+    }
 }
