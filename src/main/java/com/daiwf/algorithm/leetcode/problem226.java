fd0a5cfca4986d5d670de6d55657294f23e4fac5
戴文峰 <997940675@qq.com>
2021-11-06 09:46:27 +0800

    翻转二叉树。这个问题很简单一遍过了。Homebrew的作者大神都不会呢。哈哈哈 看来掌握基本的数据结构和递归的确是非常的重要。

--- src/main/java/com/daiwf/algorithm/leetcode/problem226.java
+++ src/main/java/com/daiwf/algorithm/leetcode/problem226.java
@@ -7,8 +7,17 @@ package com.daiwf.algorithm.leetcode;
  */
 public class problem226 {
     public TreeNode invertTree(TreeNode root) {
-
-
+        //递归中止条件
+        if (root == null) {
+            return root;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode right = root.right;
+        TreeNode left = root.left;
+        root.left = right;
+        root.right = left;
+        return root;
     }
 
     public class TreeNode {
